7a4f8fe5ba2abcccfa26f62245e41196c6ae8778
mehedi-h <mehedihs2015@gmail.com>
2023-09-13 00:21:04 +0600

    create a basic stacture with react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-xl md:text-2xl lg:text-4xl font-medium md:font-semibold lg:font-bold'>Knowledge Cafeteria</h1>
+        </div>
+    );
+};
+
+export default Header;
