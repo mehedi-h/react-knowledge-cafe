4022e122a667563679def307a1b897fa2ba4d377
mehedi-h <mehedihs2015@gmail.com>
2023-09-13 01:02:28 +0600

    modified

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -6,11 +6,11 @@ const Blogs = () => {
     useEffect( () => {
         fetch('/public/blogs.json')
         .then (res => res.json())
-        .then (data => console.log(data))
+        .then (data => setBlogs(data))
     },[])
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h2 className="text-xl md:text-3xl font-medium md:font-semibold">Blogs: {blogs.length}</h2>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <header className='flex justify-between items-center lg:p-7 lg:mx-16 border-b-2'>
             <h1 className='text-xl md:text-2xl lg:text-4xl font-medium md:font-semibold lg:font-bold'>Knowledge Cafeteria</h1>
-        </div>
+            <img src={profile} alt="Profile Photo" />
+        </header>
     );
 };
 
