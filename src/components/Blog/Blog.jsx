64bca91992abf77528ebd23d7735b69f49241cce
mehedi-h <mehedihs2015@gmail.com>
2023-09-13 14:09:11 +0600

    modified

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,29 @@
 
 
 const Blog = ({blog}) => {
+    const {title, cover, reading_time, author, author_img, posted_date,
+        hashtags} = blog
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`}/>
+            <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-x-4">
+                    <img className="w-14" src={author_img} alt="This is Author Image" />
+                    <div>
+                        <h4 className="text-base md:text-lg font-medium md:font-semibold">{author}</h4>
+                        <p><small>{posted_date}</small></p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-xl md:text-2xl font-medium md:font-semibold lg:font-bold">{title}</h2>
+            <p> 
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="#">#{hash}</a> </span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([])
@@ -11,6 +12,11 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h2 className="text-xl md:text-3xl font-medium md:font-semibold">Blogs: {blogs.length}</h2>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}></Blog>)
+            }
         </div>
     );
 };
